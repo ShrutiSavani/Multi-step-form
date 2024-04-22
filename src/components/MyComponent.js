e36aaf0cc08c559d8f3c5cf9cef82f821b9bd98a
Shruti Savani <shrutisavani2324@gmail.com>
2024-04-22 13:18:09 +0530

    Initial commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import { Text } from '@chakra-ui/react'
+import React from 'react'
+
+const MyComponent = () => {
+    return (
+        <Text>MyComponent</Text>
+    )
+}
+
+export default MyComponent
